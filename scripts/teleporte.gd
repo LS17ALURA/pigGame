937c25f9a75810bb31b74c20253de904c28d0275
felipe-ribeiro-ofc <felipe.ribeiro@alunos.ifsuldeminas.edu.br>
2023-10-05 08:51:32 -0300

    correcoes cenas

--- scripts/teleporte.gd
+++ scripts/teleporte.gd
@@ -3,6 +3,10 @@ extends Area2D
 @onready var transition = $"../transition"
 @export var next_level: String=""
 
+func _process(delta):
+	if($"/root/Itens".vida==0):
+		get_tree().change_scene_to_file("res://cenas/game_over_screen.tscn")
+
 func _on_body_entered(body):
 	if(body.name=="player" and next_level!=""):
 		transition.change_scene(next_level) # parei aqui
--- scripts/teleporte_3.gd
+++ scripts/teleporte_3.gd
@@ -1,7 +1,9 @@
 extends Area2D
 
-
-
+func _process(delta):
+	if($"/root/Itens".vida==0):
+		get_tree().change_scene_to_file("res://cenas/game_over_screen.tscn")
+	
 
 func _on_body_entered(body):
 	get_tree().change_scene_to_file("res://cenas/game_over_screen.tscn")
